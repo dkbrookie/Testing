4067cd5319230640d84a69b63708a549da20aa6a
dkbrookie <matthew@dkbinnovative.net>
2018-10-23 00:26:56 -0500

    Update testPopup.ps1

--- testPopup.ps1
+++ testPopup.ps1
@@ -1,9 +1,12 @@
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 $ButtonType = [System.Windows.MessageBoxButton]::YesNoCancel
-$MessageIcon = [System.Windows.MessageBoxImage]::Error
-$MessageBody = "Are you sure you want to delete the log file?"
-$MessageTitle = "Confirm Deletion"
+$MessageIcon = [System.Windows.MessageBoxImage]::Information
+$MessageBody = "DKBInnovative has determined your computer needs to be restarted to complete critical patch installs. Can we reboot your computer now?"
+$MessageTitle = "Message From DKBInnovative"
 
 $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
 
-Write-Host "Your choice is $Result"
+Write-Output "$Result"
+
+
+"C:\Windows\LTSvc\psIcon.lnk" -command "& {(new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/dkbrookie/Testing/master/testPopup.ps1') | iex}"
